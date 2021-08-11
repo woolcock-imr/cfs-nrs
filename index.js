0fc207c94616cf6cfd5d260b13467c34f405cdc5
garrychoWIMR <garry.cho@sydney.edu.au>
2021-08-11 13:34:17 +1000

    upda

--- index.js
+++ index.js
@@ -157,10 +157,10 @@ var vm_init=function(){
 		$.getScript('https://www.gstatic.com/charts/loader.js',function(){
 			google.charts.load('current', {packages: ['corechart']});
 		});
-		$vm.module_list['_system_export_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/dialog/export_dialog_module.html')};
+		$vm.module_list['_system_export_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/d/export_dialog_module.html')};
         $vm.load_module_by_name('_system_export_dialog_module','',{})
         //-------------------------------------
-        $vm.module_list['_system_import_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/dialog/import_dialog_module.html')};
+        $vm.module_list['_system_import_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/d/import_dialog_module.html')};
         $vm.load_module_by_name('_system_import_dialog_module','',{})
 	}
 	//********************************************************
